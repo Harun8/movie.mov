4f2ff3283d742b9df7c8b9d46a78140ab77d3777
Harun <harunabdi8@gmail.com>
2023-11-28 18:57:59 +0100

    folder clean up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import LogIn from "./routes/login";
+import SignIn from "./routes/signin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Routes>
+        <Route path="/" element={<LogIn></LogIn>}></Route>
+        <Route path="/signin" element={<SignIn></SignIn>}></Route>
+      </Routes>
     </div>
   );
 }
--- frontend/src/routes/login.js
+++ frontend/src/routes/login.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const LogIn = () => {
-  return <div>LogIn</div>;
+  return (
+    <div class="d-flex justify-content-center align-items-center vh-100">
+      <h1>hi</h1>
+    </div>
+  );
 };
 
 export default LogIn;
